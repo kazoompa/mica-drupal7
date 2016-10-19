3e5262d1ff9f0c46c212e558d17d8e1dde7bb423
samirMtl <ben.sghaier.samir@gmail.com>
2016-10-19 12:27:35 -0400

    MK-1097 Format field Information from one field is displayed in the wrong field

--- obiba_mica_study/templates/obiba_mica_study-dce-detail.tpl.php
+++ obiba_mica_study/templates/obiba_mica_study-dce-detail.tpl.php
@@ -58,7 +58,7 @@ $dce_name = obiba_mica_commons_get_localized_field($dce, 'name');
                       <li>
                         <?php print obiba_mica_commons_clean_string($dataSource); ?>
                         <?php if ($dataSource == 'others'): ?>
-                         : <?php print obiba_mica_commons_get_localized_field($dce, 'otherDataSources'); ?>
+                          :<?php print obiba_mica_commons_get_localized_field($dce, 'otherDataSources'); ?>
                         <?php endif; ?>
                       </li>
                     <?php endforeach; ?>
@@ -96,10 +96,10 @@ $dce_name = obiba_mica_commons_get_localized_field($dce, 'name');
                       <li>
                         <?php print obiba_mica_commons_clean_string($samples); ?>
                           <?php if ($samples == "tissues" && !empty($tissue_type)): ?>
-                             <?php print  $tissue_type?>
+                              :<?php print  $tissue_type?>
                           <?php endif; ?>
                         <?php if ($samples == "others" && !empty($other_bio_samples)): ?>
-                          <?php print  $other_bio_samples?>
+                           :<?php print  $other_bio_samples?>
                         <?php endif; ?>
                       </li>
                     <?php endforeach; ?>
